9c2e36f0c334b3bdb6de699155cabbc26e8ef1a3
Ar0nie <aronie@hotmail.no>
2024-06-07 12:49:48 +0200

    Added fun drag and drop feature for profile card.

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-const swup = new Swup();
-
 document.addEventListener('DOMContentLoaded', function() {
     const currentPath = window.location.pathname.split('/').pop();
     const navLinks = document.querySelectorAll('nav ul li a');
